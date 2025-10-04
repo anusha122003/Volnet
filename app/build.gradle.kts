7b15fce353c5bd347f328022a7ad0dc68215ee88
Deyona15 <silentbeauty258@gmail.com>
2025-10-04 21:51:38 +0530

    added HistoryActivity and HistoryActivity.java, dailog_custom_delete_confirmation.xml, activity_history.xml,activity_rules, bottom_nav_menu.xml,colors.xml,dailog_background.xml,export_button_background_dailog.xml,item_history_match.xml,Match1.java,MatchHistoryAdapter.java,menu_sort_options.xml,outline_arrow_back_24.xml,RulesActivity.java,search.xml,strings.xml,trophy.xml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,4 +48,6 @@ dependencies {
     implementation("com.github.bumptech.glide:glide:4.16.0")
     annotationProcessor("com.github.bumptech.glide:compiler:4.16.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.cardview:cardview:1.0.0")
 }
